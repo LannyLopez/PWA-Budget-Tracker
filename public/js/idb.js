1c74e8e51c38ff16d116aa1ff3425b1e7c647392
lanny lopez <lannylynnlopez89@gmail.com>
2022-02-28 02:44:12 -0700

    heroku fixes?

--- public/js/idb.js
+++ public/js/idb.js
@@ -31,7 +31,7 @@ let db;
 
      getAll.onsuccess = function() {
          if (getAll.result.length > 0) {
-             fetch('/api/transaction/bulk', {
+             fetch('/api/transaction', {
                  method: 'POST',
                  body: JSON.stringify(getAll.result),
                  headers: {
