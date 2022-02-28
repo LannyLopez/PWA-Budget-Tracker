c32bf80725e540daa615a7769ff47c30345d5c7b
lanny lopez <lannylynnlopez89@gmail.com>
2022-02-28 03:29:51 -0700

    readme

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
+
 # PWA-Budget-Tracker
+This is a PWA app to budget you money wether you're offline or online 
+
+## Installation
+
+Installation is really easy on this one you just clone the repo or you follow the link below
+
+https://peaceful-dawn-00841.herokuapp.com/
+## Photo of the page
+![image-example](/assets/screenshot.png)
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
+
+
--- public/js/idb.js
+++ public/js/idb.js
@@ -1,59 +1,60 @@
 let db;
 
- const request = indexedDB.open('budget', 1);
+const request = indexedDB.open('budget', 1);
 
- request.onupgradeneeded = function(event) {
-     const db = event.target.result;
-     db.createObjectStore('new_transaction', { autoIncrement: true });
- };
+request.onupgradeneeded = function(event) {
+    const db = event.target.result;
+    db.createObjectStore('new_transaction', { autoIncrement: true });
+};
 
- request.onsuccess = function(event) {
-     db = event.target.result;
-     if(navigator.onLine) {
-         uploadTransaction();
-     }
- };
+request.onsuccess = function(event) {
+    db = event.target.result;
+    if (navigator.onLine) {
+        uploadTransaction();
+    }
+};
 
- request.onerror = function(event) {
-     console.log(event.target.errorCode);
- };
+request.onerror = function(event) {
+    console.log(event.target.errorCode);
+};
 
- function saveRecord(record) {
-     const transaction = db.transaction(['new_transaction'], 'readwrite');
-     const transactionObjectStore = transaction.objectStore('new_transaction');
-     transactionObjectStore.add(record);
- }
+function saveRecord(record) {
+    const transaction = db.transaction(['new_transaction'], 'readwrite');
+    const trackerObjectStore = transaction.objectStore('new_transaction');
+    trackerObjectStore.add(record);
+};
 
- function uploadTransaction() {
-     const transaction = db.transaction(['new_transaction'], "readwrite");
-     const transactionObjectStore = transaction.objectStore("new_transaction");
-     const getAll = transactionObjectStore.getAll();
+function uploadTransaction() {
+    const transaction = db.transaction(['new_transaction'], 'readwrite');
+    const trackerObjectStore = transaction.objectStore('new_transaction');
+    const getAll = trackerObjectStore.getAll();
 
-     getAll.onsuccess = function() {
-         if (getAll.result.length > 0) {
-             fetch('/api/transaction', {
-                 method: 'POST',
-                 body: JSON.stringify(getAll.result),
-                 headers: {
-                     Accept: 'application/json, text/plain, */*',
-                     'Content-Type': 'application/json'
-                 }
-             })
-             .then((response) => response.json())
-             .them(serverResponse => {
-                 if(serverResponse.message){
-                     throw new Error(serverResponse);
-                 }
-                 const transaction = db.transaction(['new_transaction'],'readwrite');
-                 const transactionObjectStore = transaction.objectStore('new_transaction');
-                 transactionObjectStore.clear();
+    getAll.onsuccess = function() {
+        if (getAll.result.length > 0) {
+            fetch('/api/transaction', {
+                method: 'POST',
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json'
+                }
+            })
+            .then((response) => response.json())
+            .then(serverResponse => {
+                if (serverResponse.message) {
+                    throw new Error(serverResponse);
+                }
+                const transaction = db.transaction(['new_transaction'], 'readwrite');
+                const trackerObjectStore = transaction.objectStore('new_transaction');
+                trackerObjectStore.clear();
 
-                 alert('All saved transaction have been submitted');
-             }).catch(err => {
-                 console.log(err)
-             })
-         }
-     };
- }
+                alert('All saved transactions have been submitted');
+            })
+            .catch(err => {
+                console.log(err);
+            });
+        }
+    };
+};
 
- window.addEventListener('online', uploadTransaction); 
+window.addEventListener('online', uploadTransaction);
